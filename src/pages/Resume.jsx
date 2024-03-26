9d043d21a740378fe39b521b3f384043d8589f95
Ersadul-Islam <138276920+Ersadul-Islam@users.noreply.github.com>
2024-03-26 23:20:08 +0600

    cod

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,7 +9,7 @@ import pdf from "../assets/Rashed's.resume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = ``;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
 
