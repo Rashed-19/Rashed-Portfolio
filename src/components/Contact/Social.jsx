3b0e8de42d5b8475b9f5f3ba2649744286b3e9c6
Ersadul-Islam <138276920+Ersadul-Islam@users.noreply.github.com>
2024-03-29 14:57:01 +0600

    add social

--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -22,7 +22,7 @@ const Social = () => {
                 <ul className="contact-social-links">
                   <li className="contact-icons">
                     <a
-                      href="https://github.com/19sajib"
+                      href="https://github.com/Rashed-19"
                       target="_blank"
                       rel="noreferrer"
                       className="icon-color  contact-social-icons"
@@ -32,7 +32,7 @@ const Social = () => {
                   </li>
                   <li className="contact-icons">
                     <a
-                      href="https://twitter.com/19sajib"
+                      href="https://twitter.com/rashedd190"
                       target="_blank"
                       rel="noreferrer"
                       className="icon-color  contact-social-icons"
@@ -42,7 +42,7 @@ const Social = () => {
                   </li>
                   <li className="contact-icons">
                     <a
-                      href="https://www.linkedin.com/in/19sajib/"
+                      href="https://www.linkedin.com/in/rashedul19/"
                       target="_blank"
                       rel="noreferrer"
                       className="icon-color  contact-social-icons"
@@ -52,7 +52,7 @@ const Social = () => {
                   </li>
                   <li className="contact-icons">
                     <a
-                      href="hhttps://leetcode.com/19sajib/"
+                      href="https://leetcode.com/Rashed-19/"
                       target="_blank"
                       rel="noreferrer"
                       className="icon-color contact-social-icons"
